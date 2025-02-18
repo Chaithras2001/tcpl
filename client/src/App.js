ccbbaafb2156eb8a17bb1135ba7041bc2fab4d44
Chaithra Shivanna <chaithra.shivanna@tataconsumer.com>
2025-02-19 01:05:40 +0530

    chghg

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './Header
+import Header from './Header'
 import Footer from './footer';  
 
 function App() {
